ce23482db00d12f8e1cc8925cf999d6d46724f98
jiangxinqiu24-hue <jiangxinqiu24@gmail.com>
2026-02-01 17:58:13 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,9 @@
-const CACHE = "xq-life-v1";
-const ASSETS = ["./index.html", "./manifest.webmanifest", "./sw.js"];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => caches.delete(k)));
+    await self.clients.claim();
+  })());
 });
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((k) => (k === CACHE ? null : caches.delete(k))))
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((hit) => hit || fetch(e.request).then((resp) => {
-      const copy = resp.clone();
-      caches.open(CACHE).then((c) => c.put(e.request, copy));
-      return resp;
-    }).catch(()=>caches.match("./index.html")))
-  );
-});
+self.addEventListener('fetch', () => {});
